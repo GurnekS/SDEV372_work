c37d987a4002087bcf6df49c58f60265d03b8162
GurnekS <dinoman556@gmail.com>
2023-01-06 17:03:29 -0800

    Added an equals() and hashcode() method.

--- src/making_changes/Point.java
+++ src/making_changes/Point.java
@@ -1,5 +1,7 @@
 package making_changes;
 
+import java.util.Objects;
+
 public class Point
 {
     private double x;
@@ -33,5 +35,18 @@ public class Point
                 "x=" + x +
                 ", y=" + y +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return Double.compare(point.x, x) == 0 && Double.compare(point.y, y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }
